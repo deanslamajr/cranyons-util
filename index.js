c080e540a6a9b8611cebd9dc328271ec4046119f
Dean Slama Jr <deanslamajr@gmail.com>
2016-10-29 01:20:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const tinify = require('tinify');
+const nconf = require('nconf');
+
+const compressedImages = {};
+
+const constants = nconf
+  .argv()
+  .file(`${__dirname}/constants.json`);
+
+if (!nconf.get('o')) {
+  throw new Error('Must specify output filename flag: -o');
+}
+
+const outputFileName = nconf.get('o');
+
+tinify.key = constants.get('TINY_API');
+
+// Upload & compress
+compressedImages.hires = tinify.fromFile('image.jpg');
+
+// Resize
+compressedImages.pics = compressedImages.hires.resize({
+  method: 'scale',
+  width: 2000
+});
+
+compressedImages.mobile = compressedImages.hires.resize({
+  method: 'scale',
+  width: 1000
+});
+
+compressedImages.thumb = compressedImages.hires.resize({
+  method: 'scale',
+  width: 300
+});
+
+Object.keys(compressedImages).map(bucket => {
+  compressedImages[bucket].store({
+    service: 's3',
+    aws_access_key_id: constants.get('aws_access_key_id'),
+    aws_secret_access_key: constants.get('aws_secret_access_key'),
+    region: bucket === 'thumb'
+      ? constants.get('aws_region_thumbs')
+      : constants.get('aws_region'),
+    path: `${bucket}.cranyons.com/${outputFileName}.jpg`
+  });
+});
